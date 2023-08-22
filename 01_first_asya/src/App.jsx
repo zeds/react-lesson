f99e23046ea085f439d7549c3e22848a496ab0c9
anastasialit123 <aasya.lit@gmail.com>
2023-08-22 13:36:28 +0900

    styled header

--- 01_first_asya/src/App.jsx
+++ 01_first_asya/src/App.jsx
@@ -3,15 +3,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Navbar from "./components/Navbar";
+import Styled from "./pages/Styled";
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+`;
 
 function App() {
     return (
         <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/item" element={<Detail />}></Route>
-            </Routes>
+          <Container>
+              <Navbar />
+              <Routes>
+                  <Route path="/" element={<Home />}></Route>
+                  <Route path="/item" element={<Detail />}></Route>
+              </Routes>
+            </Container>
         </BrowserRouter>
     );
 }
