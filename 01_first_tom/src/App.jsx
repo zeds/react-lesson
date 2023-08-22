e88e8b44e1683477b3f1b8a9dfcf1d1886ff0951
zeds <tom.zed39@gmail.com>
2023-08-22 11:31:09 +0700

    styled header

--- 01_first_tom/src/App.jsx
+++ 01_first_tom/src/App.jsx
@@ -1,19 +1,26 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Navbar from "./components/Navbar";
 import Styled from "./pages/Styled";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+`;
 
 function App() {
     return (
         <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/item" element={<Detail />}></Route>
-                <Route path="/styled" element={<Styled />}></Route>
-            </Routes>
+            <Container>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/item" element={<Detail />}></Route>
+                    <Route path="/styled" element={<Styled />}></Route>
+                </Routes>
+            </Container>
         </BrowserRouter>
     );
 }
