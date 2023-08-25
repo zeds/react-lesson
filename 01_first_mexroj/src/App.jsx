8c540769b9175b4b76360d552dda307426781039
mexroj2603 <sayfullayevmexroj27@gmail.com>
2023-08-25 11:56:18 +0500

    detail

--- 01_first_mexroj/src/App.jsx
+++ 01_first_mexroj/src/App.jsx
@@ -1,32 +1,33 @@
-import GlobalStyle from "./GlobalStyle";
+// import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import Navbar from "./components/Navbar";
 import Styled from "./pages/Styled";
 import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
 
-const Container = styled.div`
+
+ const Container = styled.div`
     width: 100%;
     height: 100vh;
-`;
-
-// <> </>フラグメント
-
+    text-decoration: none;
+ `
+//  <>//flugment
 function App() {
     return (
         <>
-            <GlobalStyle />
-            <BrowserRouter>
-                <Container>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/item" element={<Detail />}></Route>
-                        <Route path="/styled" element={<Styled />}></Route>
-                    </Routes>
-                </Container>
-            </BrowserRouter>
+        <GlobalStyle />
+        <BrowserRouter>
+        <Container>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/item" element={<Detail />}></Route>
+                <Route path="/styled" element={<Styled />}></Route>
+            </Routes>
+            </Container>
+        </BrowserRouter>
         </>
     );
 }
