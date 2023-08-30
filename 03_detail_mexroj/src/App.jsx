164d2303438baa4760ddf729ea9f22bbb8d0e78d
mexroj2603 <sayfullayevmexroj27@gmail.com>
2023-08-30 08:30:14 +0500

    03

---
+++ 03_detail_mexroj/src/App.jsx
@@ -0,0 +1,35 @@
+// import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Navbar from "./components/Navbar";
+import Styled from "./pages/Styled";
+import styled from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+
+
+ const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    text-decoration: none;
+ `
+//  <>//flugment
+function App() {
+    return (
+        <>
+        <GlobalStyle />
+        <BrowserRouter>
+        <Container>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/item" element={<Detail />}></Route>
+                <Route path="/styled" element={<Styled />}></Route>
+            </Routes>
+            </Container>
+        </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
