aa7a3c73284f8aad09acfc9fa6302d5a2d7f272e
tienduongbn <0981672106bn@gmail.com>
2023-10-13 19:06:33 +0900

    2023/10/13

--- 14_redux_neko_tien/src/redux/slices/store.tsx
+++ 14_redux_neko_tien/src/redux/slices/store.tsx
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import lightSlice from "./lightSlice";
 
-export const store = configureStore({
-    reducer: {
-        //store.light đối chiếu đến slice
-        light:lightSlice,
+  export const rootReducer = combineReducers({
+
+    //   reducer: {
+    //       //store.light đối chiếu đến slice
+          light:lightSlice,
     }
 })
