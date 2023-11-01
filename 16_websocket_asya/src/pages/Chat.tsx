842b68d8d12cffc0519398287695900577bffc82
unknown <aasya.lit@gmail.com>
2023-11-01 11:05:06 +0900

    chat lS

--- 16_websocket_asya/src/pages/Chat.tsx
+++ 16_websocket_asya/src/pages/Chat.tsx
@@ -4,22 +4,22 @@ import styled from "styled-components";
 import { PulseLoader } from "react-spinners";
 
 const override: CSSProperties = {
-	display: "block",
-	position: "absolute",
-	margin: "0 auto",
-	borderColor: "red",
+  display: "block",
+  position: "absolute",
+  margin: "0 auto",
+  borderColor: "red",
 };
 
 const Frame = styled.div`
-	width: 100%;
-	height: 100vh;
-	background: lightblue;
-	padding-top: 64px;
+  width: 100%;
+  height: 100vh;
+  background: lightblue;
+  padding-top: 64px;
 `;
 
 type Chat = {
-	name: string;
-	text: string;
+  name: string;
+  text: string;
 };
 
 type ChatLog = Array<Chat>;
@@ -27,153 +27,146 @@ type ChatLog = Array<Chat>;
 //接続
 const socket = io("http://localhost:3443");
 // const socket = io("http://danang-alley.com:3443");
+
 const Chat = () => {
-	const [chatLog, setChatLog] = useState<ChatLog>([]);
-	const [name, setName] = useState<string>("");
-	const [text, setText] = useState<string>("");
-	const [joined, setJoined] = useState(false);
-	const [typingDisplay, setTypingDisplay] = useState(false);
-	const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [chatLog, setChatLog] = useState<ChatLog>(() => {
+    // Попытка загрузить данные чата из localStorage при загрузке компонента
+    const savedChatLog = localStorage.getItem("chatLog");
+    return savedChatLog ? JSON.parse(savedChatLog) : [];
+  });
+  const [name, setName] = useState<string>("");
+  const [text, setText] = useState<string>("");
+  const [joined, setJoined] = useState(false);
+  const [typingDisplay, setTypingDisplay] = useState(false);
+  const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-	// useEffect(() => {
-	// 	//接続が完了したら、発火
-	// 	socket.on("connect", () => {
-	// 		console.log("接続ID : ", socket.id);
-	// 	});
+  useEffect(() => {
+    socket.emit("typing", { isTyping: true });
 
-		
-	// }, []);
+    if (timerRef.current) {
+      window.clearTimeout(timerRef.current);
+    }
 
-	useEffect(() => {
-		console.log("text=", text);
-		socket.emit("typing", { isTyping: true });
+    timerRef.current = setTimeout(() => {
+      socket.emit("typing", { isTyping: false });
+    }, 2000);
+  }, [text]);
 
-		if (timerRef.current) {
-			window.clearTimeout(timerRef.current);
+  useEffect(() => {
+	console.log("useEffectで登録サーバーから初期値を取得");
+
+    socket.emit("findAllMessages", (chat: any) => {
+      setChatLog(chat);
+		console.log("chat受信", chat);
+		console.log("chat受信", chatLog);
+    });
+
+    socket.on("message", (message) => {
+      setChatLog((current) => {
+        const updatedChatLog = [...current, message];
+        // Сохранение данных чата в localStorage
+        localStorage.setItem("chatLog", JSON.stringify(updatedChatLog));
+        return updatedChatLog;
+      });
+    });
+
+    socket.on("typing", ({ name, isTyping }) => {
+      if (isTyping) {
+        setTypingDisplay(true);
+      } else {
+        setTypingDisplay(false);
+      }
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("createMessage", { name: name, text: text }, () => {
+      setText("");
+    });
+  };
+
+  const join = (event: any) => {
+    event.preventDefault();
+    socket.emit("join", { name: name }, () => {
+      setJoined(true);
+	  localStorage.setItem("userName", name);
+    });
+  };
+
+  useEffect(() => {
+	const savedUserName = localStorage.getItem("userName");
+		if (savedUserName) {
+		setName(savedUserName);
+		setJoined(true);
 		}
+  	}, []);
 
-		timerRef.current = setTimeout(() => {
-			socket.emit("typing", { isTyping: false });
-		}, 2000);
-	}, [text]);
+  return (
+    <Frame>
+      {joined ? (
+        <div>
+          <span>お名前：</span>
+          <span>{name}</span>
+          <br />
 
-	useEffect(() => {
-		console.log("useEffectで登録サーバーから初期値を取得");
+          <div className="chat_container">
+            <div className="message_container">
+              {chatLog.map((item: any, index) => (
+                <div key={index}>
+                  <span>{item.name}</span>
+                  <span>：{item.text}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>送信内容</div>
+          <div>
+            <span>メッセージ</span>
+            <input
+              type="text"
+              value={text}
+              onChange={(event) => {
+                setText(event.target.value);
+              }}
+              placeholder="メッセージ"
+            />
+          </div>
+          <br />
+          <div>
+            <button onClick={sendMessage}> send </button>
+          </div>
+          {typingDisplay ? (
+            <PulseLoader
+              cssOverride={override}
+              size={50}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          ) : (
+            <div></div>
+          )}
+        </div>
+      ) : (
+        <form onSubmit={(event) => join(event)}>
+          <label>What's your name?</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            placeholder="お名前"
+          />
 
-		socket.emit("findAllMessages", (chat: any) => {
-			setChatLog(chat);
-			console.log("chat受信", chat);
-			console.log("chat受信", chatLog);
-		});
-
-		socket.on("message", (message) => {
-			console.log("message=", message);
-			setChatLog((current) => [...current, message]);
-			console.log(chatLog);
-		});
-		socket.on("typing", ({ name, isTyping }) => {
-			console.log("誰かが入力してます");
-			if (isTyping) {
-				console.log("typing");
-				setTypingDisplay(true);
-			} else {
-				setTypingDisplay(false);
-			}
-		});
-		
-		//切断
-		return () => {
-			console.log("切断");
-			socket.disconnect();
-		};
-	}, []);
-
-	const sendMessage = () => {
-		socket.emit("createMessage", { name: name, text: text }, () => {
-			//TODO: clear name, text
-			setText("");
-		});
-	};
-
-	const join = (event: any) => {
-		event.preventDefault();
-		socket.emit("join", { name: name }, () => {
-			setJoined(true);
-		});
-	};
-
-	//現在時刻取得
-	// const getNow = useCallback((): string => {
-	// 	const datetime = new Date();
-	// 	return `${datetime.getFullYear()}/${
-	// 		datetime.getMonth() + 1
-	// 	}/${datetime.getDate()} ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`;
-	// }, []);
-
-	return (
-		<Frame>
-			{joined ? (
-				<div>
-					<span>お名前：</span>
-					<span>{name}</span>
-					<br />
-
-					<div className="chat_container">
-						<div className="message_container">
-							{chatLog.map((item: any, index) => (
-								<div key={index}>
-									<span>{item.name}</span>
-									<span>：{item.text}</span>
-								</div>
-							))}
-						</div>
-					</div>
-					<div>送信内容</div>
-					<div>
-						<span>メッセージ</span>
-						<input
-							type="text"
-							value={text}
-							onChange={(event) => {
-								setText(event.target.value);
-							}}
-							placeholder="メッセージ"
-						/>
-					</div>
-					<br />
-					<div>
-						<button onClick={sendMessage}> send </button>
-					</div>
-					{typingDisplay ? (
-						<PulseLoader
-							// color={color}
-							// loading={loading}
-							cssOverride={override}
-							size={50}
-							aria-label="Loading Spinner"
-							data-testid="loader"
-						/>
-					) : (
-						<div></div>
-					)}
-				</div>
-			) : (
-				<form onSubmit={(event) => join(event)}>
-					<label>What's your name?</label>
-					<input
-						type="text"
-						value={name}
-						onChange={(event) => {
-							setName(event.target.value);
-						}}
-						placeholder="お名前"
-					/>
-
-					<button type="submit">送信</button>
-				</form>
-			)}
-		</Frame>
-	);
+          <button type="submit">送信</button>
+        </form>
+      )}
+    </Frame>
+  );
 };
 
 export default Chat;
