a3478c0c6efa06bac803709c1d4bb3cef6ffc3d9
zeds <tom.zed39@gmail.com>
2023-11-06 11:15:47 +0700

    chat

--- 16_websocket_asya/src/App.tsx
+++ 16_websocket_asya/src/App.tsx
@@ -8,28 +8,26 @@ import Users from "./pages/Users";
 import Mypage from "./pages/Mypage";
 import Chat from "./pages/Chat";
 import ChatHomePage from "./pages/ChatHomePage";
-import ChatRoomPage from './pages/ChatRoomPage';
-import { UserProvider } from './pages/UserContext';
-
+import ChatRoomPage from "./pages/ChatRoomPage";
+import { UserProvider } from "./pages/UserContext";
 
 function App() {
 	return (
 		<>
 			<UserProvider>
 				<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/mypage" element={<Mypage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/users" element={<Users />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/chat" element={<Chat />} />
-				<Route path="/chatHome" element={<ChatHomePage />} />
-        		<Route path="/chat/:roomName" element={<ChatRoomPage />} />
-			</Routes>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/mypage" element={<Mypage />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/users" element={<Users />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/chat" element={<ChatRoomPage />} />
+					<Route path="/chatHome" element={<ChatHomePage />} />
+					<Route path="/chat/:roomName/:name" element={<ChatRoomPage />} />
+				</Routes>
 			</UserProvider>
-			
 		</>
 	);
 }
--- 16_websocket_asya/src/pages/ChatHomePage.tsx
+++ 16_websocket_asya/src/pages/ChatHomePage.tsx
@@ -4,57 +4,62 @@ import styled from "styled-components";
 import { useUser } from "./UserContext";
 
 const Container = styled.div`
-  text-align: center;
-  padding: 20px;
+	text-align: center;
+	padding: 20px;
 `;
 
 const Title = styled.h1`
-  font-size: 24px;
-  margin-bottom: 10px;
+	font-size: 24px;
+	margin-bottom: 10px;
 `;
 
 const Label = styled.label`
-  display: block;
-  margin: 10px 0;
+	display: block;
+	margin: 10px 0;
 `;
 
 const Input = styled.input`
-  width: 100%;
-  padding: 5px;
+	width: 100%;
+	padding: 5px;
 `;
 
 const RoomButton = styled.button`
-  margin: 5px;
+	margin: 5px;
 `;
 
 const ChatHomePage = () => {
-  const { userName, setUserName } = useUser();
-  const [selectedRoom, setSelectedRoom] = useState("");
+	const { userName, setUserName } = useUser();
+	const [selectedRoom, setSelectedRoom] = useState("");
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
-  };
-  
+	const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setUserName(event.target.value);
+	};
 
-  return (
-    <Container>
-      <Title>Welcome to the Chat Page</Title>
-      <Label>Enter your name:</Label>
-      <Input type="text" value={userName} onChange={handleNameChange} />
-      <div>
-        <p>Select a room:</p>
-        <Link to={`/chat/roomA`}>
-          <RoomButton onClick={() => setSelectedRoom("roomA")}>Room A</RoomButton>
-        </Link>
-        <Link to={`/chat/roomB`}>
-          <RoomButton onClick={() => setSelectedRoom("roomB")}>Room B</RoomButton>
-        </Link>
-        <Link to={`/chat/roomC`}>
-          <RoomButton onClick={() => setSelectedRoom("roomC")}>Room C</RoomButton>
-        </Link>
-      </div>
-    </Container>
-  );
+	return (
+		<Container>
+			<Title>Welcome to the Chat Page</Title>
+			<Label>Enter your name:</Label>
+			<Input type="text" value={userName} onChange={handleNameChange} />
+			<div>
+				<p>Select a room:</p>
+				<Link to={`/chat?room=roomA&name=${userName}`}>
+					<RoomButton onClick={() => setSelectedRoom("roomA")}>
+						Room A
+					</RoomButton>
+				</Link>
+				<Link to={`/chat?room=roomB&name=${userName}`}>
+					<RoomButton onClick={() => setSelectedRoom("roomB")}>
+						Room B
+					</RoomButton>
+				</Link>
+				<Link to={`/chat?room=roomC&name=${userName}`}>
+					<RoomButton onClick={() => setSelectedRoom("roomC")}>
+						Room C
+					</RoomButton>
+				</Link>
+			</div>
+		</Container>
+	);
 };
 
 export default ChatHomePage;
